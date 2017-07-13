5a9726cfc8c7548c257c26f3d56a6c58715dc46a
Peter Donald <peter@realityforge.org>
2017-07-13 17:57:24 +1000

    Add standard license banner

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,17 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 $:.unshift File.expand_path('../../lib', __FILE__)
 
 require 'securerandom'
@@ -45,6 +59,6 @@ class Reality::Idea::TestCase < Minitest::Test
   end
 
   def workspace_dir
-    @workspace_dir ||= ENV['TEST_TMP_DIR'] || File.expand_path("#{File.dirname(__FILE__)}/../tmp/workspace")
+    @workspace_dir ||= File.expand_path(ENV['TEST_TMP_DIR'] || "#{File.dirname(__FILE__)}/../tmp/workspace")
   end
 end
