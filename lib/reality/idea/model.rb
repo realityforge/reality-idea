acf2b15904b3943004d7619d7a3566e67474d161
Peter Donald <peter@realityforge.org>
2017-07-20 09:55:11 +1000

    Name elements consistently

--- lib/reality/idea/model.rb
+++ lib/reality/idea/model.rb
@@ -24,7 +24,7 @@ module Reality
         r.model_element(:project)
         r.model_element(:project_component, :project, :access_method => :components, :inverse_access_method => :component)
         r.model_element(:ruby_module, :project)
-        r.model_element(:ruby_component, :ruby_module, :access_method => :components, :inverse_access_method => :component)
+        r.model_element(:ruby_module_component, :ruby_module, :access_method => :components, :inverse_access_method => :component)
       end
     end
   end
